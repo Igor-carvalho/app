f285f8726025b03d7f3c9c2e2daea9ba7997a48f
Shaheryar Abid <shaheryar93@live.com>
2018-04-28 11:25:04 +0500

    testing js code for the wishlist page

--- frontend/src/app/wishlist/wishlist.component.ts
+++ frontend/src/app/wishlist/wishlist.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {TweenMax, Power2, TimelineLite, TweenLite} from "gsap";
+import {TweenMax, Power2, Back, Power0, Elastic, Bounce, SplitText, TimelineLite, TweenLite, CSSPlugin, EasePack} from "gsap";
+import * as $ from 'jquery';
 
 @Component({
     selector: 'app-wishlist',
@@ -14,5 +15,11 @@ export class WishlistComponent implements OnInit {
 
     ngOnInit() {
         TweenLite.to("#heading", 2, {rotation:360});
+        
     }
+    
+    addPeople(event) {
+        console.log(event.target.id);
+    }
+
 }

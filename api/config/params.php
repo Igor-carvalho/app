fcff872c97b9d8fa1fc86fbb11cf2a020ee36320
Abdullah Mateen <abdullahmateen87@gmail.com>
2018-06-20 14:12:02 +0300

    Added New Frontend Url

--- api/config/params.php
+++ api/config/params.php
@@ -1,9 +1,20 @@
 <?php
 
-return [
-    'frontendURL'   => 'http://dobedoo.acorn-labs.com/frontend/',
-    'supportEmail'  =>  'info@dobedoo.it',
-    'adminEmail'    => 'admin@dobedoo.it',
-    'jwtSecretCode' =>  'someSecretKey',
-    'user.passwordResetTokenExpire' => 3600,
-];
+if (YII_DEBUG) {
+    return [
+        'frontendURL' => 'http://localhost:4201/',
+        'supportEmail' => 'info@dobedoo.it',
+        'adminEmail' => 'admin@dobedoo.it',
+        'jwtSecretCode' => 'someSecretKey',
+        'user.passwordResetTokenExpire' => 3600,
+    ];
+} else {
+    return [
+        'frontendURL' => 'http://app.dobedoo.it/',
+        'supportEmail' => 'info@dobedoo.it',
+        'adminEmail' => 'admin@dobedoo.it',
+        'jwtSecretCode' => 'someSecretKey',
+        'user.passwordResetTokenExpire' => 3600,
+    ];
+}
+
